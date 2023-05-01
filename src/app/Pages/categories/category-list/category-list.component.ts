898c232879d54d964ccc02d16d25982ef3c70e4c
Thiago Teixeira <thiagoemecal@gmail.com>
2023-05-01 20:58:09 -0300

    finalizando a Categoria

--- src/app/Pages/categories/category-list/category-list.component.ts
+++ src/app/Pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Category} from "../Shared/category.model"
+import { CategoryService } from '../Shared/category.service';
+import { error } from 'console';
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  constructor(private categoryService : CategoryService) { }
 
   ngOnInit() {
+
+    this.categoryService.getAll().subscribe (
+      categories => this.categories  = categories, 
+      error=> alert('Erro ao carregar a lista')
+    )
+  }
+
+  deleteCategory (category)
+  {
+    const mustDelete =  confirm('Deseja realmente excluir este item '+ category.name );
+
+    if(mustDelete)
+    console.log('entrou');
+    this.categoryService.delete(category.id).subscribe(
+      () => this.categories = this.categories.filter(element => element != category),
+      () => alert('erro ao tentar excluir')
+    ) 
   }
 
 }
